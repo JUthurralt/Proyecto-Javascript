8b823a4e8c518dc4b07476f37add31c1ca9bb484
Julian Uthurralt <julianuthurralt@gmail.com>
2021-10-13 17:10:12 -0300

    DOM

--- Lupetto Bebidas/app.js
+++ Lupetto Bebidas/app.js
@@ -23,75 +23,75 @@ const producto4 = new Producto("Boris ACDC", "Cerveza", 450, "Imagenes/Cerveza.j
 
 let productosArray = [producto1, producto2, producto3, producto4]
 
-// if (localStorage.getItem("productos") == null) {
-//     productosArray = []
-// } else {
-//     productosArray = JSON.parse(localStorage.getItem("producto"))
-// }
+if (localStorage.getItem("productos") == null) {
+    productosArray = []
+} else {
+    productosArray = JSON.parse(localStorage.getItem("producto"))
+}
 
-// let total = 0
-// let carrito = []
-// for (let i = 0; i<50; i++){
-//     let respuestaCliente = parseInt(prompt("¿Qué bebida desea llevar?: 1) Whisky 2) Gin 3) Ron 4) Cerveza 5) Salir 6) Vaciar Carrito"));
-//     if (respuestaCliente == 1) {
-//         producto1.mensaje();
-//         carrito.push(producto1);
-//         console.log(carrito);
-//         sumarPrecioAlTotal(producto1);
-//     }
-//     else if (respuestaCliente == 2) {
-//         producto2.mensaje();
-//         carrito.push(producto2);
-//         console.log(carrito);
-//         sumarPrecioAlTotal(producto2);
-//     }
-// else if (respuestaCliente == 3) {
-//     producto3.mensaje();
-//     carrito.push(producto3);
-//     console.log(carrito);
-//     sumarPrecioAlTotal(producto3);
-//     }
-// else if (respuestaCliente == 4) {
-//     producto4.mensaje();
-//     carrito.push(producto4);
-//     console.log(carrito);
-//     sumarPrecioAlTotal(producto4);
-//     }
-// else if (respuestaCliente == 5) {
-//     alert ("El total de tu carrito es " + total + ". Tu carrito contiene " + carrito.length + " productos." );
-//     if (total == 0) {
-//         alert("No seleccionaste ningún producto.")
-//         parseInt(prompt("¿Qué bebida desea llevar?: 1) Whisky 2) Gin 3) Ron 4) Cerveza 5) Salir 6) Vaciar Carrito"))
-//     }
-//     else { 
-//     let metodoDePago = prompt("¿Cómo desea abonar la compra? E) Efectivo (10% de descuento) T) Tarjeta de Crédito o Débito (10% de recargo).")
-//     if (metodoDePago == "E") {
-//             total = total * 0.90;
-//             alert ("El total es " + total + ". Por pagar en efectivo se le realiza un 10% de descuento. ¡Muchas gracias por su compra!")
-//             break;
-//             }
-//         else if (metodoDePago == "T") {
-//             total = total * 1.10;
-//             alert ("El total es " + total + ". Por pagar con tarjeta se le aplica un recargo del 10%. ¡Muchas Gracias por su compra!");
-//             break;
-//             }
+let total = 0
+let carrito = []
+for (let i = 0; i<50; i++){
+    let respuestaCliente = parseInt(prompt("¿Qué bebida desea llevar?: 1) Whisky 2) Gin 3) Ron 4) Cerveza 5) Salir 6) Vaciar Carrito"));
+    if (respuestaCliente == 1) {
+        producto1.mensaje();
+        carrito.push(producto1);
+        console.log(carrito);
+        sumarPrecioAlTotal(producto1);
+    }
+    else if (respuestaCliente == 2) {
+        producto2.mensaje();
+        carrito.push(producto2);
+        console.log(carrito);
+        sumarPrecioAlTotal(producto2);
+    }
+else if (respuestaCliente == 3) {
+    producto3.mensaje();
+    carrito.push(producto3);
+    console.log(carrito);
+    sumarPrecioAlTotal(producto3);
+    }
+else if (respuestaCliente == 4) {
+    producto4.mensaje();
+    carrito.push(producto4);
+    console.log(carrito);
+    sumarPrecioAlTotal(producto4);
+    }
+else if (respuestaCliente == 5) {
+    alert ("El total de tu carrito es " + total + ". Tu carrito contiene " + carrito.length + " productos." );
+    if (total == 0) {
+        alert("No seleccionaste ningún producto.")
+        parseInt(prompt("¿Qué bebida desea llevar?: 1) Whisky 2) Gin 3) Ron 4) Cerveza 5) Salir 6) Vaciar Carrito"))
+    }
+    else { 
+    let metodoDePago = prompt("¿Cómo desea abonar la compra? E) Efectivo (10% de descuento) T) Tarjeta de Crédito o Débito (10% de recargo).")
+    if (metodoDePago == "E") {
+            total = total * 0.90;
+            alert ("El total es " + total + ". Por pagar en efectivo se le realiza un 10% de descuento. ¡Muchas gracias por su compra!")
+            break;
+            }
+        else if (metodoDePago == "T") {
+            total = total * 1.10;
+            alert ("El total es " + total + ". Por pagar con tarjeta se le aplica un recargo del 10%. ¡Muchas Gracias por su compra!");
+            break;
+            }
 
 
-//         }
-//     }
+        }
+    }
 
     
-// else if (respuestaCliente == 6) {
-//     carrito = [];
-//     total = 0;
-//     console.log(carrito);
-//     alert ("Tu carrito esta vacío.");
-//     } 
+else if (respuestaCliente == 6) {
+    carrito = [];
+    total = 0;
+    console.log(carrito);
+    alert ("Tu carrito esta vacío.");
+    } 
 
-// else {
-//     alert ("La opción ingresada es incorrecta.");
-// }
-// }
+else {
+    alert ("La opción ingresada es incorrecta.");
+}
+}
 
 // DESAFIO COMPLEMENTARIO CLASE 6
 const menor = productosArray.reverse(producto => producto.precio);
