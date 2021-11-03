18cced342eed2701c456e2e81e59e19fe12509df
Julian Uthurralt <julianuthurralt@gmail.com>
2021-11-03 20:56:19 -0300

    3era entrega

--- Lupetto Bebidas/app.js
+++ Lupetto Bebidas/app.js
@@ -1,117 +1,70 @@
 // FUNCIONES
 function sumarPrecioAlTotal(producto) {
-    total += producto.precio;
+  total += producto.precio;
 }
 
-function tabla(valorSeleccionado, total) {
-    $("#tabla").append(`<tr>
-    <td>${valorSeleccionado.id}</td>
+function tabla(valorSeleccionado, newTotal) {
+  $("#tabla").append(`
+    <tr>
+    <td>1</td>
     <td>${valorSeleccionado.tipo}</td>
     <td>${valorSeleccionado.precio}</td>
-    <td>${total}</td>
+    <td class="td">${total}</td>
     <td><button class="borrar" value="${valorSeleccionado.id}">X</button></td>
-    </tr>`)
+    </tr>`);
+}
+function actualizarTotal() {
+  let td = document.getElementsByClassName("td");
+  for (const td1 of td) td1.innerText = `${total}`;
 }
 
-// function logIn()
+// function logIn()}
 
-class Producto{
-    constructor(marca, tipo, precio, img, id) {
-        this.marca = marca;
-        this.tipo = tipo;
-        this.precio = precio;
-        this.img = img;
-        this.id = id;
-    }
-    mensaje(){
-        console.log("Agregaste "+ this.tipo + ", a un precio de " + this.precio);
-    }
+class Producto {
+  constructor(marca, tipo, precio, img, id) {
+    this.marca = marca;
+    this.tipo = tipo;
+    this.precio = precio;
+    this.img = img;
+    this.id = id;
+  }
+  mensaje() {
+    console.log("Agregaste " + this.tipo + ", a un precio de " + this.precio);
+  }
 }
-const producto1 = new Producto("Johnnie Walker", "Whisky", 6500, "Imagenes/Whisky.png", 1);
-const producto2 = new Producto("Beefeater", "Gin", 3500, "Imagenes/Gin.jpeg", 2);
+const producto1 = new Producto(
+  "Johnnie Walker",
+  "Whisky",
+  6500,
+  "Imagenes/Whisky.png",
+  1
+);
+const producto2 = new Producto(
+  "Beefeater",
+  "Gin",
+  3500,
+  "Imagenes/Gin.jpeg",
+  2
+);
 const producto3 = new Producto("Bacardi", "Ron", 2900, "Imagenes/Ron.jpeg", 3);
-const producto4 = new Producto("Boris ACDC", "Cerveza", 450, "Imagenes/Cerveza.jpeg", 4);
+const producto4 = new Producto(
+  "Boris ACDC",
+  "Cerveza",
+  450,
+  "Imagenes/Cerveza.jpeg",
+  4
+);
 
-let productosArray = [producto1, producto2, producto3, producto4]
+let productosArray = [producto1, producto2, producto3, producto4];
 
-let total = 0
-let carrito = []
-// for (let i = 0; i<50; i++){
-//     let respuestaCliente = parseInt(prompt("¿Qué bebida desea llevar?: 1) Whisky 2) Gin 3) Ron 4) Cerveza 5) Salir 6) Vaciar Carrito"));
-//     if (respuestaCliente == 1) {
-//         producto1.mensaje();
-//         carrito.push(producto1);
-//         console.log(carrito);
-//         sumarPrecioAlTotal(producto1);
-//     }
-//     else if (respuestaCliente == 2) {
-//         producto2.mensaje();
-//         carrito.push(producto2);
-//         console.log(carrito);
-//         sumarPrecioAlTotal(producto2);
-//     }
-// else if (respuestaCliente == 3) {
-//     producto3.mensaje();
-//     carrito.push(producto3);
-//     console.log(carrito);
-//     sumarPrecioAlTotal(producto3);
-//     }
-// else if (respuestaCliente == 4) {
-//     producto4.mensaje();
-//     carrito.push(producto4);
-//     console.log(carrito);
-//     sumarPrecioAlTotal(producto4);
-//     }
-// else if (respuestaCliente == 5) {
-//     alert ("El total de tu carrito es " + total + ". Tu carrito contiene " + carrito.length + " productos." );
-//     if (total == 0) {
-//         alert("No seleccionaste ningún producto.")
-//         parseInt(prompt("¿Qué bebida desea llevar?: 1) Whisky 2) Gin 3) Ron 4) Cerveza 5) Salir 6) Vaciar Carrito"))
-//     }
-//     else { 
-//     let metodoDePago = prompt("¿Cómo desea abonar la compra? E) Efectivo (10% de descuento) T) Tarjeta de Crédito o Débito (10% de recargo).")
-//     if (metodoDePago == "E") {
-//             total = total * 0.90;
-//             alert ("El total es " + total + ". Por pagar en efectivo se le realiza un 10% de descuento. ¡Muchas gracias por su compra!")
-//             break;
-//             }
-//         else if (metodoDePago == "T") {
-//             total = total * 1.10;
-//             alert ("El total es " + total + ". Por pagar con tarjeta se le aplica un recargo del 10%. ¡Muchas Gracias por su compra!");
-//             break;
-//             }
-
-
-//         }
-//     }
-
-    
-// else if (respuestaCliente == 6) {
-//     carrito = [];
-//     total = 0;
-//     console.log(carrito);
-//     alert ("Tu carrito esta vacío.");
-//     } 
-
-// else {
-//     alert ("La opción ingresada es incorrecta.");
-// }
-// }
-
-// DESAFIO COMPLEMENTARIO CLASE 6
-// const menor = productosArray.reverse(producto => producto.precio);
-// console.log(menor);
-
-// const iva = productosArray.map(producto => producto.precio * 1.21);
-// console.log(iva);
-
-//DESAFIO CLASE 8 DOM
+let total = 0;
+let carrito = [];
 
 let seccionProducto = "";
 let contenedorProductos = document.getElementById("fotos-productos");
 
-productosArray.forEach(element => {
-    seccionProducto += `<div id="producto${element.id}" class="card" style="width: 18rem;">
+productosArray.forEach((element) => {
+  seccionProducto += `<div id="producto${element.id}" class="card" style="width: 18rem;">
     <div class="card-body">
       <h4 class="card-title">${element.marca}</h4>
       <h5 class="card-subtitle mb-2 text-muted">${element.tipo}</h5>
@@ -119,90 +72,101 @@ productosArray.forEach(element => {
       <p class="card-text">$${element.precio}</p>
       <button class="card-link" value=${element.id}>Comprar</button>
     </div>
-  </div>`
+  </div>`;
 });
 
 contenedorProductos.innerHTML = seccionProducto;
 
-// INCORPORANDO EVENTOS - CLASE 9
 let botones = document.getElementsByClassName("card-link");
 console.log(botones);
 
+let clicks = 0;
+for (const boton of botones) {
+  boton.addEventListener("click", function (e) {
+    e.stopImmediatePropagation();
+    let valorSeleccionado = productosArray.find((x) => x.id == e.target.value);
+    carrito.push(valorSeleccionado);
+    total += valorSeleccionado.precio;
+    tabla(valorSeleccionado, total);
 
-for (const boton of botones){
-    boton.addEventListener("click", function (e){
+    let remover = document.querySelector(".alert");
+    remover.innerHTML = `<p class="p">${valorSeleccionado.tipo} se agregó al carrito.</p>`;
+
+    $("#cartel").css("display", "flex");
+    $("#cartel").slideUp(4000);
+
+    let botonesBorrar = document.getElementsByClassName("borrar");
+    for (const botonesB of botonesBorrar) {
+      botonesB.addEventListener("click", function (e) {
         e.stopImmediatePropagation();
-        let valorSeleccionado = productosArray.find(x => x.id == e.target.value);
-        carrito.push(valorSeleccionado);
-        console.log(carrito);
-        total += valorSeleccionado.precio;
-        console.log(total);
-        tabla(valorSeleccionado, total);
-       
-        let botonesBorrar = document.getElementsByClassName("borrar");
-        for (const botonesB of botonesBorrar){
-            botonesB.addEventListener("click", function(e){
-                botonesB.parentNode.parentNode.remove();
-            })
-        }   
-    })
-          
+        let valorSeleccionado1 = productosArray.find(
+          (x) => x.id == e.target.value
+        );
+        let item = carrito.indexOf(valorSeleccionado1);
+        carrito.splice(item, 1);
+        total = total - valorSeleccionado1.precio;
+        botonesB.parentNode.parentNode.remove();
+        actualizarTotal();
+      });
+    }
+  });
 }
-// FORMULARIO
 
+$("#enviar").click((e) => {
+  e.preventDefault();
 
+  let usuario = document.getElementById("user").value;
+  localStorage.setItem("nombreUsuario", JSON.stringify(usuario));
 
+  let mail = document.getElementById("email").value;
+  localStorage.setItem("mail", JSON.stringify(mail));
 
-$("#enviar").click(()=> {
+  const infoPost = {"nombre": usuario , "mail": mail}
+  const APIURL = "https://jsonplaceholder.typicode.com/posts"
 
-// let enviar = document.getElementById("enviar");
-// enviar.addEventListener("click", function(e){
-    e.preventDefault();
-
-    let usuario = document.getElementById("user").value;
-    localStorage.setItem("nombreUsuario", JSON.stringify(usuario));
-    
-    let contra = document.getElementById("pwd").value;
-    localStorage.setItem("contrasenia", JSON.stringify(contra));
-
-    
-    console.log(JSON.parse(localStorage.getItem("nombreUsuario")));
-    console.log(JSON.parse(localStorage.getItem("contrasenia")));
+$.ajax({
+  method: "POST",
+  url:  APIURL,
+  data: infoPost,
+  success: function(respuesta){
+    console.log(respuesta.nombre , respuesta.mail)
+      $("#registro").append(`<p>Gracias por registrarse, ${respuesta.nombre}. Le llegarán nuestras a ofertas a ${respuesta.mail}.</p>`)
+  }
+});
 })
+
+
+
+
+
+
+
+
+
 
 // COMPLEMENTARIO CLASE 9
 function carritoPrueba(total) {
-let totalCarrito = document.getElementById("total-carrito");
-console.log(totalCarrito);
-let p = document.createElement("p");
-p.innerHTML=`<p>El total de su compra es $${total}</p>`;
-totalCarrito.appendChild(p);
+  let totalCarrito = document.getElementById("total-carrito");
+  console.log(totalCarrito);
+  let p = document.createElement("p");
+  p.innerHTML = `<p>El total de su compra es $${total}</p>`;
+  totalCarrito.appendChild(p);
 }
-$("#finalizarCompra").click(function(e){
-    carritoPrueba(total);
-})
+$("#finalizarCompra").click(function (e) {
+  carritoPrueba(total);
+});
 // let botonFinalizar = document.getElementById("finalizarCompra");
 // botonFinalizar.addEventListener("click", function (e){
 //     carritoPrueba(total);
 // });
 
-// TABLA
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$("#clickCarrito").click(function (e) {
+  if (clicks == 0) {
+    $(".contenedorTabla").slideDown(1000);
+    $(".contenedorTabla").css("display", "flex");
+    clicks++;
+  } else {
+    $(".contenedorTabla").slideUp(1000);
+    clicks--;
+  }
+});
